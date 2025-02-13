5f1bf82f4ba09fc351601a8b9ceb11ba79c7ee06
Svetlana Kalashnikova <serebsveta@rambler.ru>
2025-02-13 22:16:47 +0800

    fixing

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,11 +1,11 @@
+from src.category import Category
+
+
 def test_category_init_1(first_category, second_category):
     """Тест класса Category"""
     assert first_category.name == "Смартфоны"
     assert first_category.description == "Смартфоны, как средство коммуникации"
     assert len(first_category.products) == 3
 
-    assert first_category.category_count == 2
-    assert second_category.category_count == 2
-
-    assert first_category.product_count == 7
-    assert second_category.product_count == 7
+    assert Category.category_count == 2
+    assert Category.product_count == 7
