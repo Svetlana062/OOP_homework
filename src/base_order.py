397fc97838f3644f4c4661c1d84185b07896cc18
Svetlana Kalashnikova <serebsveta@rambler.ru>
2025-03-02 15:47:26 +0800

    Implemented class and abstract class order

---
+++ src/base_order.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+class BaseOrder(ABC):
+    """Базовый абстрактный класс, родительский для класса Order и Category."""
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    @abstractmethod
+    def __str__(self):
+        """Строковое представление объекта."""
+        pass
---
+++ src/order.py
@@ -0,0 +1,18 @@
+from src.base_order import BaseOrder
+from src.product import Product
+
+class Order(BaseOrder):
+    """Класс для заказа, который содержит информацию о товаре и его количестве."""
+
+    def __init__(self, product: Product, quantity: int):
+        super().__init__(name=product.name, description=product.description)
+        self.product = product
+        self.quantity = quantity
+        self.total_price = self.calculate_total()
+
+    def calculate_total(self):
+        """Метод для расчета общей стоимости заказа."""
+        return self.product.price * self.quantity
+
+    def __str__(self):
+        return f"Заказ: {self.name}, Количество: {self.quantity}, Итоговая стоимость: {self.total_price} руб."
