e93391cfd9d1c5647c7a3a4156895a2d2e424819
Svetlana Kalashnikova <serebsveta@rambler.ru>
2025-03-07 00:57:49 +0800

    changes

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from src.category import Category
 from src.product import Product
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
-        product_invalid = Product("Бракованный товар", "Неверное количество", 1000.0, 0)
-    except ValueError as e:
+        product_invalid = Product("Бракованный товар", "Неверное количество", 1000.0, 0, "Оранжевый")
+    except ValueError:
         print(
-            "Возникла ошибка ValueError прерывающая работу программы при попытке добавить продукт с нулевым количеством")
+            "Возникла ошибка ValueError прерывающая работу программы при попытке добавить продукт с нулевым "
+            "количеством"
+        )
     else:
         print("Не возникла ошибка ValueError при попытке добавить продукт с нулевым количеством")
 
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, "Зеленый")
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8, "Черный")
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14, "Синий")
 
     category1 = Category("Смартфоны", "Категория смартфонов", [product1, product2, product3])
 
